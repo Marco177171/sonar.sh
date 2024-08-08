58594a9c1ea7e3edbf4e1f7698bc14c749cec579
Marco177171 <msebastiani93@gmail.com>
2024-08-08 15:50:08 +0200

    refactor

--- sonar.sh
+++ sonar.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
+echo "SONAR | a simple encrypted messenger"
+echo "made with <3 by studio pulsar"
+echo "INITIAL SETUP"
 read -p "username: " NICK
-read -p "password: " PASSWORD
 read -p "insert nc server port: " PORT
 read -p "insert destination's port: " DESTINATION
+read -p "password: " PASSWORD
+echo SETUP COMPLETE
+
+clean() {
+	echo sigint felt in my little hearty
+}
 
 listen() {
-while true
-do
-nc -l $PORT | openssl aes-256-cbc -md sha512 -d -a -pbkdf2 -pass pass:$PASSWORD -iter 1000000
-done
+	while true
+	do
+		nc -l $PORT | openssl aes-256-cbc -md sha512 -d -a -pbkdf2 -pass pass:$PASSWORD
+	done
 }
 
 write() {
-while true
-do
-read MESSAGE
-echo $NICK: $MESSAGE | openssl aes-256-cbc -md sha512 -a -pbkdf2 -salt -pass pass:$PASSWORD -iter 1000000 | nc -N localhost $DESTINATION
-done
+	while true
+	do
+		read MESSAGE
+		echo $NICK: $MESSAGE | openssl aes-256-cbc -md sha512 -a -pbkdf2 -salt -pass pass:$PASSWORD | nc -N localhost $DESTINATION
+	done
 }
 
 listen &
